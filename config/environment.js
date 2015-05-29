27a4a86662dd1087d2601dd6d9f3d1ea69d8bd38
Alex Martin <martin.alex.t+github@gmail.com>
2015-05-28 23:07:21 -0400

    Update environments

--- config/environment.js
+++ config/environment.js
@@ -30,7 +30,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.baseURL = '/final_fontjam';
     ENV.locationType = 'none';
 
     // keep test console output quieter
@@ -41,7 +41,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/final_fontjam/';
+    ENV.baseURL = '/final_fontjam';
   }
 
   return ENV;
